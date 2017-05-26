6fec5b5b1a2f33f690231425803f5d604baf9c6f
ScarletBlue <koreanboy1120@gmail.com>
2017-05-26 12:19:32 +0900

    Working on Dynamic water new method

---
+++ Assets/MeshOrderChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshOrderChange : MonoBehaviour
+{
+
+	public MeshRenderer _renderer;
+	// Use this for initialization
+	void Update()
+	{
+
+		_renderer.sortingLayerName = "LayerName";
+		_renderer.sortingOrder = 100;
+	}
+
+}
