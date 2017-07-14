3f46ae23dc3886faf089470f27be74b0c25a2aed
Juhyeong Park <majecty@gmail.com>
2017-07-14 19:05:11 +0900

    Rename StoryTeller to old storyteller

--- Assets/Script/InGame/StoryTelling/OldStoryTeller.cs
+++ Assets/Script/InGame/StoryTelling/OldStoryTeller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StoryTeller : MonoBehaviour
+public class OldStoryTeller : MonoBehaviour
 {
     // public string text;
     public float coolTime;
